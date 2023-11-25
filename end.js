6ac1f7aa36beeb937c9989db5f79288a1f99669b
irfan <72319855+irfankurtagic@users.noreply.github.com>
2023-11-25 12:16:34 +0100

    Add files via upload

--- end.js
+++ end.js
@@ -25,5 +25,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/Philosophy Quiz/index.html');
+    window.location.assign('/philosophy-quiz/index.html');
 };
